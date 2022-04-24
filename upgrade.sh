de1cf64f61aff9ec9cf32e8003fc14aef9265b60
Julien Mousqueton <julien@mousqueton.io>
2022-04-24 15:24:56 +0200

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,8 +1,106 @@
 #!/bin/bash 
 #Invoice Ninja Self-Hosted Upgrade
 
+unknown_os ()
+{
+  echo "Unfortunately, your operating system distribution and version are not supported by this script."
+  echo
+  echo "You can override the OS detection by setting os= and dist= prior to running this script."
+  echo "You can find a list of supported OSes and distributions on our website: https://packagecloud.io/docs#os_distro_version"
+  echo
+  echo "For example, to force Ubuntu Trusty: os=ubuntu dist=trusty ./script.sh"
+  echo
+  echo "Please email support@packagecloud.io and let us know if you run into any issues."
+  exit 1
+}
+
+mlocate_check ()
+{
+  echo "Checking for curl..."
+  if command -v mlocate > /dev/null; then
+    echo "Detected mlocate..."
+  else
+    echo "Installing mlocate..."
+    apt-get install -q -y mlocate
+    if [ "$?" -ne "0" ]; then
+      echo "Unable to install mlocate ! Your base system has a problem; please check your default OS's package repositories because mlocate should work."
+      echo "Repository installation aborted."
+      exit 1
+    fi
+  fi
+}
+
+detect_os ()
+{
+  if [[ ( -z "${os}" ) && ( -z "${dist}" ) ]]; then
+    # some systems dont have lsb-release yet have the lsb_release binary and
+    # vice-versa
+    if [ -e /etc/lsb-release ]; then
+      . /etc/lsb-release
+
+      if [ "${ID}" = "raspbian" ]; then
+        os=${ID}
+        dist=`cut --delimiter='.' -f1 /etc/debian_version`
+      else
+        os=${DISTRIB_ID}
+        dist=${DISTRIB_CODENAME}
+
+        if [ -z "$dist" ]; then
+          dist=${DISTRIB_RELEASE}
+        fi
+      fi
+
+    elif [ `which lsb_release 2>/dev/null` ]; then
+      dist=`lsb_release -c | cut -f2`
+      os=`lsb_release -i | cut -f2 | awk '{ print tolower($1) }'`
+
+    elif [ -e /etc/debian_version ]; then
+      # some Debians have jessie/sid in their /etc/debian_version
+      # while others have '6.0.7'
+      os=`cat /etc/issue | head -1 | awk '{ print tolower($1) }'`
+      if grep -q '/' /etc/debian_version; then
+        dist=`cut --delimiter='/' -f1 /etc/debian_version`
+      else
+        dist=`cut --delimiter='.' -f1 /etc/debian_version`
+      fi
+
+    else
+      unknown_os
+    fi
+  fi
+
+  if [ -z "$dist" ]; then
+    unknown_os
+  fi
+
+  # remove whitespace from OS and dist name
+  os="${os// /}"
+  dist="${dist// /}"
+
+  echo "Detected operating system as $os/$dist."
+}
+
+detect_version_id () {
+  # detect version_id and round down float to integer
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    version_id=${VERSION_ID%%.*}
+  elif [ -f /usr/lib/os-release ]; then
+    . /usr/lib/os-release
+    version_id=${VERSION_ID%%.*}
+  else
+    version_id="1"
+  fi
+}
+
+#CHECK OS
+#--------------------------------------------------------
+#detect_os
+#detect_version_id
+
 #SET INVOICE NINJA INSTALL PATH
 #--------------------------------------------------------
+mlocate_check
 updatedb
 ninja_home="$(locate -b '\composer.json' | xargs grep -l "invoiceninja/invoiceninja" | xargs -n 1 dirname)"
 
